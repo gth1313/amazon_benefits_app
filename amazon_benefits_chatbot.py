94e6ae73e43bde7dda47f8e38d7a5deef5c0ec7f
Tinghao Guo <guotinghao@yahoo.com>
2024-06-10 14:21:43 -0500

    add openai_api_key

--- amazon_benefits_chatbot.py
+++ amazon_benefits_chatbot.py
@@ -2,13 +2,14 @@ from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.vectorstores import FAISS
-
+import os
 import streamlit as st
 
 def query(question, chat_history):
-    embeddings = OpenAIEmbeddings()
+    openai_api_key = os.environ.get("OPEN_AI_KEY")
+    embeddings = OpenAIEmbeddings(api_key = openai_api_key)
     new_db = FAISS.load_local("faiss_amazon_index", embeddings, allow_dangerous_deserialization=True)
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key = openai_api_key)
 
     query = ConversationalRetrievalChain.from_llm(
         llm=llm, 
